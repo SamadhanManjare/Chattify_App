335421f7bcce105e220088930f505b110ae6eba6
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-10 20:32:44 +0530

    added bcrypt in auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-
-export const signup = (req, res) => {
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+export const signup = async (req, res) => {
     
     const { fullname, email, password } = req.body;
 
@@ -17,9 +18,34 @@ export const signup = (req, res) => {
         if(!emailRegex.test(email)) {
             return res.status(400).json({ message: 'Invalid email format' });
         }
-         
+         const user = await User.findOne({ email });
+        if(user) {
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            fullname,
+            email,
+            password: hashedPassword,
+        });
+        if(newUser){
+            generateToken(newUser._id, res);
+            await newUser.save();
+             return res.status(201).json({ 
+               
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+             });
+        }else{
+            return res.status(500).json({ message: 'Error creating user' });
+        }
+
     } catch (error) {
         console.error("Error during signup:", error);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: 'Email Already Exists' });
     }
 }
