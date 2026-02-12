65fc634594c153808c6feb7fc85b1f1bb32aaabd
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-12 16:59:06 +0530

    Build controller for login

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -70,3 +70,35 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;  
+    
+    try{
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ message: 'Invalid email or password' });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) return res.status(400).json({ message: 'Invalid email or password' });
+
+        generateToken(user._id, res);
+
+        return res.status(200).json({
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+        
+
+    }
+    catch(error){
+        console.error("Error during login:", error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const logout = (req, res) => {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
+}
