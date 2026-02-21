ffaa5e6d780af10c8692e9b2814803540e573fe0
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-21 23:18:17 +0530

    worked on pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
 
 function App() {
   return (
-    <div>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className="btn btn-primary">Click me</button>
+   <Routes>
       
-    </div>
+      <Route path='/' element={<ChatPage/>} />
+    </Routes>
   )
 }
 
