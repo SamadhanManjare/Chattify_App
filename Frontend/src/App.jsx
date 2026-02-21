702620d88dc5795a9fe1dddce5f2fa7cd7ae23bc
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-21 23:12:51 +0530

    implemented react router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
           <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button className="btn btn-primary">Click me</button>
       
     </div>
   )
