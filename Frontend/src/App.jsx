203045de588f708bf54a81ae39d3c04d5903f0aa
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-24 17:10:11 +0530

    frontend setup of Zustand and react router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,42 @@
-import React from 'react'
 
-import { Routes, Route } from 'react-router'
+
+import { Routes, Route } from 'react-router-dom'
 import ChatPage from './pages/ChatPage' 
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
-import { useAuthStore } from './store/useAuthStore'
+import useAuthStore  from './store/useAuthStore'
 
 
 
 function App() {
 
   const {authUser, isLoggedIn, login} = useAuthStore();
+
     console.log("Auth User : ", authUser);
     console.log("isLoggedIn :", isLoggedIn);
   return (
-    <>
-    
-    <div className='absolute inset-0 -z-10 opacity-20'>
-
-    
-      
-       {/* DECORATORS - GRID BG & GLOW SHAPES */}
-      
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] " />
+  
+      <div className="min-h-screen bg-slate-900 relative flex flex-col items-center justify-center gap-4 p-4 overflow-hidden text-white">
+        <h1 className="text-3xl font-bold">Welcome to Chattify App</h1>
+         {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-     
 
-     
-        
-      <Routes>
-        
+      <button onClick={login} className='z-10'>Login</button>
+
+    <Routes>
         <Route path='/' element={<ChatPage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/signup' element={<SignupPage/>} />
       </Routes>
-    </div>
+
+      </div>
+      
+
+   
     
-   </>
-  )
-}
+   )}
+
 
 export default App;
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,12 +1,13 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
     authUser: {name : 'John Doe', _id : '12345',age: '25'},
     isLoggedIn : false,
+    isLoading : false,
 
     login : () => {
         console.log("We just logged in....")
-        set({isLoggedIn : true});
+        set({isLoggedIn : true, isLoading : true});
     }
 }));
 export default useAuthStore;
