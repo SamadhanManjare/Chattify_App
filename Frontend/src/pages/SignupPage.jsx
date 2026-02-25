aa7783aeedc8ee88aedadb5ccede7bcb9e6d0cd6
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-25 22:38:37 +0530

    added msg Icon and added comment

--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useAuthStore  from '../store/useAuthStore'
 import { useState } from 'react';
 import BorderAnimatedContainer from '../components/BorderAnimatedContainer';
+import { MessageCircleIcon } from '@heroicons/react/24/outline';
 
 function Signup() {
 
@@ -19,10 +20,13 @@ function Signup() {
             {/* Left side with image and text */}
             <div className='md:w-1/2 w-full h-full flex items-center justify-center p-8 md:border-r border-gray-700'>
               <div className='w-full max-w-md'>
+                {/* Headinng text */}
                 <MessageCircleIcon className='w-12 h-12 mx-auto mb-4 text-slate-400' />
                 <h2 className='text-2xl font-bold text-slate-400 mb-4'>Create Account</h2>
-                <p className=' text-slate-300'>Join our community and start chatting!</p>
+                <p className=' text-slate-300'>Signup to start chatting with your friends!</p>
               </div>
+              {/* form */}
+              <form action=""></form>
             </div>
              </div>
         </BorderAnimatedContainer>
